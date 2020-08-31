c6da30181c1e2ab129852406e74c1424dbf84f8c
larsvens <larsvens@kth.se>
2020-08-31 13:10:36 +0200

    mvp global plan ifc, loc and state est

--- src/xt28_localization/scripts/global_plan_to_yaml.py
+++ src/xt28_localization/scripts/global_plan_to_yaml.py
@@ -8,14 +8,10 @@ Usage:
 """
 
 import rosbag
+import rospkg
 import matplotlib.pyplot as plt
 import Tkinter, tkFileDialog
 import yaml
-
-from scipy import interpolate
-from lxml import etree 
-import re
-
 
 plt.close('all')
 
@@ -43,8 +39,13 @@ path =	{
   "lon": lon
 }
 
+plt.plot(lon,lat)
+plt.show()
+
+filename = "latest.yaml"
+filepath = rospkg.RosPack().get_path('xt28_localization') + '/data/global_plans_yaml/' 
 #f = open("sample.yaml", "w")
-with open("sample.yaml", 'w') as outfile:
+with open(filepath + filename, 'w') as outfile:
     yaml.dump(path, outfile)
 
 
@@ -58,52 +59,54 @@ with open("sample.yaml", 'w') as outfile:
 #        print(utm_nr)
 #        print(utm_letter)
         
+#      from scipy import interpolate
+#    from lxml import etree 
+#    import re  
         
         
-        
-    def get_cl_from_kml(self,filepath):    
-        tree = etree.parse(filepath)    
-        lineStrings = tree.findall('.//{http://www.opengis.net/kml/2.2}LineString')    
-        for attributes in lineStrings:
-            for subAttribute in attributes:
-                if subAttribute.tag == '{http://www.opengis.net/kml/2.2}coordinates':
-                    coords = subAttribute.text
-        
-        # clean and cast
-        coords = re.split(',| ',coords)
-        coords[0] = coords[0].translate(None, "\n\t\t\t\t")
-        coords = coords[:-1]
-        
-        # split in lon lat and alt
-        if(len(coords) % 3 != 0):
-            print "Error: len(coords) not divisible by three"
-        lat = []
-        lon = []
-        alt = []
-        for i in range(len(coords)/3):
-            lon.append(float(coords[3*i]))
-            lat.append(float(coords[3*i+1]))
-            alt.append(float(coords[3*i+2]))
-        lat = np.array(lat)
-        lon = np.array(lon)
-        alt = np.array(alt)
-        
-        # convert to utm
-        X_utm,Y_utm,utm_nr,utm_letter = utm.from_latlon(lat, lon)
-        
-        # interpolate 
-        ds = 1.0 # want approx 1 m between pts
-        # approximate stot
-        stot = 0
-        for i in range(X_utm.size-1):
-            stot += np.sqrt((X_utm[i+1]-X_utm[i])**2 + (Y_utm[i+1]-Y_utm[i])**2)
-        N = int(stot/ds)
-        unew = np.arange(0, 1.0, 1.0/N) # N equidistant pts
-        tck, u = interpolate.splprep([X_utm, Y_utm], s=0)
-        out = interpolate.splev(unew, tck)
-        X_utm_out = out[0]
-        Y_utm_out = out[1]    
-    
-        return X_utm_out, Y_utm_out, utm_nr, utm_letter 
+#    def get_cl_from_kml(self,filepath):    
+#        tree = etree.parse(filepath)    
+#        lineStrings = tree.findall('.//{http://www.opengis.net/kml/2.2}LineString')    
+#        for attributes in lineStrings:
+#            for subAttribute in attributes:
+#                if subAttribute.tag == '{http://www.opengis.net/kml/2.2}coordinates':
+#                    coords = subAttribute.text
+#        
+#        # clean and cast
+#        coords = re.split(',| ',coords)
+#        coords[0] = coords[0].translate(None, "\n\t\t\t\t")
+#        coords = coords[:-1]
+#        
+#        # split in lon lat and alt
+#        if(len(coords) % 3 != 0):
+#            print "Error: len(coords) not divisible by three"
+#        lat = []
+#        lon = []
+#        alt = []
+#        for i in range(len(coords)/3):
+#            lon.append(float(coords[3*i]))
+#            lat.append(float(coords[3*i+1]))
+#            alt.append(float(coords[3*i+2]))
+#        lat = np.array(lat)
+#        lon = np.array(lon)
+#        alt = np.array(alt)
+#        
+#        # convert to utm
+#        X_utm,Y_utm,utm_nr,utm_letter = utm.from_latlon(lat, lon)
+#        
+#        # interpolate 
+#        ds = 1.0 # want approx 1 m between pts
+#        # approximate stot
+#        stot = 0
+#        for i in range(X_utm.size-1):
+#            stot += np.sqrt((X_utm[i+1]-X_utm[i])**2 + (Y_utm[i+1]-Y_utm[i])**2)
+#        N = int(stot/ds)
+#        unew = np.arange(0, 1.0, 1.0/N) # N equidistant pts
+#        tck, u = interpolate.splprep([X_utm, Y_utm], s=0)
+#        out = interpolate.splev(unew, tck)
+#        X_utm_out = out[0]
+#        Y_utm_out = out[1]    
+#    
+#        return X_utm_out, Y_utm_out, utm_nr, utm_letter 
         
         
--- src/xt28_localization/scripts/xt28_loc_node.py
+++ src/xt28_localization/scripts/xt28_loc_node.py
@@ -14,6 +14,7 @@ import rospy
 import rospkg
 from nav_msgs.msg import Path as Path
 from geometry_msgs.msg import PoseStamped
+from visualization_msgs.msg import Marker
 import tf
 
 # custom msgs
@@ -28,18 +29,22 @@ import utm
 # misc
 import yaml
 
-
-
 class LocalizationNode:
     # constructor
     def __init__(self):
         # init node
         rospy.init_node('xt28_localization', anonymous=True)
         self.dt = rospy.get_param('/dt_localization')
+        self.dt_gp = rospy.get_param('/dt_pathglobal')
         self.rate = rospy.Rate(1./self.dt)      
 
-        # pubs and subs
-        self.pathglobalpub = rospy.Publisher('pathglobal_vis', Path, queue_size=1)
+        # timing
+        self.pathglobal_pub_time_ratio = int(self.dt_gp/self.dt)
+
+        # pubs and subs etc
+        self.tfbr = tf.TransformBroadcaster()
+        self.pathglobalpub = rospy.Publisher('pathglobal', Path, queue_size=1)
+        self.statetextmarkerpub = rospy.Publisher('state_text_marker', Marker, queue_size=1)
         self.gnsssub = rospy.Subscriber("/fix", GNSSFix, self.gnss_callback)
         self.fix = GNSSFix()
         self.received_gnss = False
@@ -51,11 +56,11 @@ class LocalizationNode:
             path = yaml.load(f,Loader=yaml.SafeLoader)
 
         # convert to utm
-        X_utm,Y_utm,utm_nr,utm_letter = utm.from_latlon(np.array(path["lat"]), np.array(path["lon"]))
+        Xgp_utm,Ygp_utm,utm_nr,utm_letter = utm.from_latlon(np.array(path["lat"]), np.array(path["lon"]))
     
         # set path in map coord sys (filter to ca 1pt/m)
-        X_map_tmp = X_utm - X_utm[0]
-        Y_map_tmp = Y_utm - Y_utm[0]
+        X_map_tmp = Xgp_utm - Xgp_utm[0]
+        Y_map_tmp = Ygp_utm - Ygp_utm[0]
         #X_map_tmp = np.append(X_map_tmp, X_map_tmp[0]) # for closed path only
         #Y_map_tmp = np.append(Y_map_tmp, Y_map_tmp[0])
     
@@ -68,9 +73,9 @@ class LocalizationNode:
         
         # resample with equidistant points
         ds = 1.0
-        s = np.arange(0,s_tmp[-1],ds)
-        X_map = np.interp(s,s_tmp,X_map_tmp)
-        Y_map = np.interp(s,s_tmp,Y_map_tmp)
+        s_map = np.arange(0,s_tmp[-1],ds)
+        X_map = np.interp(s_map,s_tmp,X_map_tmp)
+        Y_map = np.interp(s_map,s_tmp,Y_map_tmp)
         
         # compute psic
         dX = np.diff(X_map)
@@ -81,11 +86,11 @@ class LocalizationNode:
         psic = np.append(psic,psic_final)         
      
         # set utm origin pose from first entry and tangent in global plan 
-        X0_utm = float(X_utm[0])
-        Y0_utm = float(Y_utm[0])
+        X0_utm = float(Xgp_utm[0])
+        Y0_utm = float(Ygp_utm[0])
         psi0_utm = float(np.arctan2(Y_map[1]-Y_map[0],X_map[1]-X_map[0])) # map and utm has same orientation and since X/Y_map has 1m distance this is more robust
                 
-        origin_pose_utm =	{
+        self.origin_pose_utm =	{
           "X0_utm": X0_utm,
           "Y0_utm": Y0_utm,
           "psi0_utm": psi0_utm,
@@ -93,28 +98,70 @@ class LocalizationNode:
           "utm_letter": utm_letter
         }
         
-        # publish global plan as path
-        pathglobalvis = Path()
-        for i in range(X_map.size):
-            pose = PoseStamped()
-            pose.header.stamp = rospy.Time.now()
-            pose.header.frame_id = "map"
-            pose.pose.position.x = X_map[i]
-            pose.pose.position.y = Y_map[i]            
-            quaternion = tf.transformations.quaternion_from_euler(0, 0, psic[i])
-            pose.pose.orientation.x = quaternion[0]
-            pose.pose.orientation.y = quaternion[1]
-            pose.pose.orientation.z = quaternion[2]
-            pose.pose.orientation.w = quaternion[3]
-            pathglobalvis.poses.append(pose)
-        pathglobalvis.header.stamp = rospy.Time.now()
-        pathglobalvis.header.frame_id = "map"
-        #rospy.logwarn("track_iface: publishing pathglobal visualization")
-        self.pathglobalpub.publish(pathglobalvis)
-
+        # check
+        print X_map.size
 
         # Main loop
+        count  = 0
         while not rospy.is_shutdown():
+            
+            # get transform map-->base_link
+            if(not self.received_gnss):
+                rospy.logwarn_throttle(1.0,"xt28_loc: waiting for gnss message")
+            else:
+                X_utm, Y_utm, utm_nr, utm_letter = utm.from_latlon(self.fix.latitude, self.fix.longitude)
+                X_raw = X_utm - self.origin_pose_utm["X0_utm"]
+                Y_raw = Y_utm - self.origin_pose_utm["Y0_utm"]
+                psi_raw = self.fix.heading
+                
+                # check utm zone
+                if(utm_nr != self.origin_pose_utm["utm_nr"] or utm_letter != self.origin_pose_utm["utm_letter"]):
+                    rospy.logerr("UTM zone mismatch: GPS measurement utm_nr =     " + str(utm_nr) + ", origin_pose utm_nr =     " + str(self.origin_pose_utm["utm_nr"]))
+                    rospy.logerr("UTM zone mismatch: GPS measurement utm_letter = " + utm_letter + ", origin_pose utm_letter = " + str(chr(self.origin_pose_utm["utm_letter"])))                    
+
+                # publish transform
+                self.tfbr.sendTransform((X_raw, Y_raw, 0),
+                        tf.transformations.quaternion_from_euler(0, 0, psi_raw),
+                        rospy.Time.now(),
+                        "base_link",
+                        "map")
+            
+                # get s and d position relative to pathglobal
+                s, d = self.ptsCartesianToFrenet(np.array([X_raw]),np.array([Y_raw]),X_map,Y_map,psic,s_map)
+                rospy.logwarn("xt28_loc: s = " + str(s))
+                rospy.logwarn("xt28_loc: d = " + str(d))
+            
+                # publish text marker
+                state_text = "s:     " + "%.3f" % s + "\n"  \
+                             "d:     " + "%.3f" % d 
+                m = self.get_state_text_marker(state_text)
+                m.header.stamp = rospy.Time.now()
+                self.statetextmarkerpub.publish(m) 
+            
+            # publish global path (at lower rate) 
+            if(count >= self.pathglobal_pub_time_ratio):
+                pathglobalvis = Path()
+                for i in range(X_map.size):
+                    pose = PoseStamped()
+                    pose.header.stamp = rospy.Time.now()
+                    pose.header.frame_id = "map"
+                    pose.pose.position.x = X_map[i]
+                    pose.pose.position.y = Y_map[i]            
+                    quaternion = tf.transformations.quaternion_from_euler(0, 0, psic[i])
+                    pose.pose.orientation.x = quaternion[0]
+                    pose.pose.orientation.y = quaternion[1]
+                    pose.pose.orientation.z = quaternion[2]
+                    pose.pose.orientation.w = quaternion[3]
+                    pathglobalvis.poses.append(pose)
+                pathglobalvis.header.stamp = rospy.Time.now()
+                pathglobalvis.header.frame_id = "map"
+                self.pathglobalpub.publish(pathglobalvis)
+
+                count = 0
+            else:
+                count +=1
+            
+            
             #rospy.logwarn("xt28_loc: in main loop")
             self.rate.sleep()
                         
@@ -122,8 +169,68 @@ class LocalizationNode:
     def gnss_callback(self, msg):
         self.fix = msg
         self.received_gnss = True
-        rospy.logwarn("xt28_loc: in callback")
+        
+
+    
+    def ptsCartesianToFrenet(self,X,Y,Xpath,Ypath,psipath,spath):
+        # inputs and outputs are np.array([x])
+    
+        Npts = X.size
+        s = np.zeros(Npts);
+        d = np.zeros(Npts);
+        
+        # transform pts one at a time
+        for k in range(Npts):
+        
+            # find closest pt on centerline
+            dist = np.sqrt((X[k]-Xpath)**2 + (Y[k]-Ypath)**2 )
+            idx = np.argmin(dist)
+    
+            # make sure deltas is positive
+            deltas = -1
+            maxiters = 5
+            iters = 0
+            while(deltas < 0 and iters <= maxiters):
             
+                Xc = Xpath[idx] 
+                Yc = Ypath[idx] 
+                psic = psipath[idx] 
+                sc = spath[idx] 
+                
+                # compute angles etc
+                deltaX = X - Xc
+                deltaY = Y - Yc
+                alpha1 = -np.arctan2(deltaX,deltaY)
+                alpha2 = psic - alpha1
+                M = np.sqrt(deltaX**2 + deltaY**2)
+                deltas = M*np.sin(alpha2)
+                
+                # iteratively reduce idx until deltas positive
+                idx = idx - 1
+                iters = iters + 1
+                
+            d[k] = M*np.cos(alpha2)
+            s[k] = sc + deltas
+            
+        return s,d 
+ 
+    def get_state_text_marker(self,text):
+        m = Marker()
+        m.header.frame_id = "base_link"
+        m.pose.position.x = 0.0;
+        m.pose.position.y = 0.0;
+        m.pose.position.z = 10.0;
+        m.type = m.TEXT_VIEW_FACING;
+        m.text = text
+        m.scale.x = 2.0;
+        m.scale.y = 2.0;
+        m.scale.z = 2.0;
+        m.color.a = 1.0; 
+        m.color.r = 0.0;
+        m.color.g = 0.0;
+        m.color.b = 0.0;
+        return m
+           
 
 if __name__ == '__main__':
     ln = LocalizationNode()
